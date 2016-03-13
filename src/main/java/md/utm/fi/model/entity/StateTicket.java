bbfb5618c2b4c626477b3238240c78729b928995
olgagherman <gherman.olya@gmail.com>
2016-03-13 22:08:11 +0200

    entities

--- src/main/java/md/utm/fi/model/entity/StateTicket.java
+++ src/main/java/md/utm/fi/model/entity/StateTicket.java
@@ -1,12 +1,18 @@
 package md.utm.fi.model.entity;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class StateTicket {
 
 	private Integer id;
 	private String name;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	public Integer getId() {
 		return id;
 	}
--- src/main/java/md/utm/fi/model/entity/Ticket.java
+++ src/main/java/md/utm/fi/model/entity/Ticket.java
@@ -3,6 +3,9 @@ package md.utm.fi.model.entity;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Ticket {
@@ -12,6 +15,9 @@ public class Ticket {
 	private String subject;
 	private Integer complexity;
 	private Date created;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	public Integer getId() {
 		return id;
 	}
