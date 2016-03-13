fc1bd3d668fe1f8f1286620d321b07da88de8dc9
olgagherman <gherman.olya@gmail.com>
2016-03-13 22:03:33 +0200

    added entity state ticket

---
+++ src/main/java/md/utm/fi/model/entity/StateTicket.java
@@ -0,0 +1,23 @@
+package md.utm.fi.model.entity;
+
+import javax.persistence.Entity;
+
+@Entity
+public class StateTicket {
+
+	private Integer id;
+	private String name;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
