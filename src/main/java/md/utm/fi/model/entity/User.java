d2df2da77399bbd00bde4fb168edc405795cec24
grigore-94 <gr.brodicico@gmail.com>
2016-03-13 21:53:31 +0200

    User entity

--- src/main/java/md/utm/fi/model/entity/User.java
+++ src/main/java/md/utm/fi/model/entity/User.java
@@ -1,5 +1,73 @@
 package md.utm.fi.model.entity;
 
-public class User {
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
+public class User extends Person {
+
+	private Integer id;
+	private String email;
+	private String password;
+	private String phoneNumber;
+	private Date createdDate;
+	private boolean admin;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Temporal(TemporalType.DATE)
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public boolean isAdmin() {
+		return admin;
+	}
+
+	public void setAdmin(boolean admin) {
+		this.admin = admin;
+	}
 
 }
