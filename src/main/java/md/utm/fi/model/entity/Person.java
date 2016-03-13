9561bf18ea0743ae8e4cd4ec36cb45c103ca80cf
grigore-94 <gr.brodicico@gmail.com>
2016-03-13 21:45:54 +0200

    person

--- src/main/java/md/utm/fi/model/entity/Person.java
+++ src/main/java/md/utm/fi/model/entity/Person.java
@@ -1,5 +1,26 @@
 package md.utm.fi.model.entity;
 
-public class Person {
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class Person {
+	private String name;
+	private String surname;
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
 }
