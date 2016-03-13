c9b714f93579378fd5827b78a2678052cc77105e
grigore-94 <gr.brodicico@gmail.com>
2016-03-14 00:31:55 +0200

    action

---
+++ src/main/java/md/utm/fi/action/user/CrudUserAction.java
@@ -0,0 +1,94 @@
+package md.utm.fi.action.user;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import com.opensymphony.xwork2.Action;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ModelDriven;
+
+import md.utm.fi.model.dao.UserDAO;
+import md.utm.fi.model.entity.User;
+
+public class CrudUserAction implements ModelDriven<User> {
+
+	private User user = new User();
+
+	private UserDAO userDAO;
+
+	private List<User> userList;
+
+	private Integer userId;
+
+	public List<User> getUserList() {
+		return userList;
+	}
+
+	public void setUserList(List<User> userList) {
+		this.userList = userList;
+	}
+
+	public User getModel() {
+		return user;
+	}
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	public UserDAO getUserDAO() {
+		return userDAO;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String addUser() throws Exception {
+		user.setCreatedDate(new Date());
+		userDAO.save(user);
+		if (user.getName() != null) {
+			return Action.SUCCESS;
+		}
+		return Action.ERROR;
+	}
+
+	public String updateUser() {
+		userDAO.saveOrUpdate(user);
+		return Action.SUCCESS;
+	}
+
+	@SuppressWarnings("unchecked")
+	public String editUser() {
+		String returnType = Action.NONE;
+		if (userId != null) {
+			ActionContext.getContext().getValueStack().getRoot().remove(user);
+			user = getUserDAO().findUser(userId);
+			ActionContext.getContext().getValueStack().getRoot().add(user);
+			returnType = Action.SUCCESS;
+		}
+		return returnType;
+	}
+
+	public String removeUser() {
+		if (userId != null) {
+			getUserDAO().deleteUser(userId);
+		}
+		return Action.SUCCESS;
+	}
+
+	// list all users
+	public String listAllUsers() {
+		userList = userDAO.getAllUsers();
+		if (userList == null) {
+			userList = new ArrayList<User>();
+		}
+		return Action.SUCCESS;
+	}
+
+}
---
+++ src/main/java/md/utm/fi/action/user/LoginAuthAction.java
@@ -0,0 +1,81 @@
+package md.utm.fi.action.user;
+
+import java.util.Map;
+
+import org.apache.struts2.interceptor.SessionAware;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+import md.utm.fi.model.dao.UserDAO;
+import md.utm.fi.model.entity.User;
+
+public class LoginAuthAction extends ActionSupport implements SessionAware {
+
+	private static final long serialVersionUID = 1L;
+	private String email;
+	private String password;
+	private UserDAO userDAO;
+
+	private Boolean error;
+	private Map<String, Object> sessionMap;
+	private User user;
+
+	@Override
+	public String execute() {
+		if (email.equals("")) {
+			addActionError("Please Enter user email.");
+			error = true;
+		} else if (password.equals("")) {
+			addActionError("Please Enter password.");
+			error = true;
+		} else if (email.equalsIgnoreCase(password)) {
+			addActionError("Email and password should be different.");
+			error = true;
+		} else if (findUser() == null) {
+			error = true;
+		} else {
+			error = false;
+			sessionMap.put("userName", user.getName());
+			sessionMap.put("logged", true);
+			sessionMap.put("admin", user.isAdmin());
+		}
+
+		if (error) {
+			return ERROR;
+		} else {
+			return SUCCESS;
+		}
+
+	}
+
+	public String logOut() {
+		if (sessionMap.containsKey("userName")) {
+			sessionMap.remove("userName");
+			sessionMap.put("logged", false);
+			sessionMap.put("admin", false);
+		}
+		return SUCCESS;
+	}
+
+	private User findUser() {
+		user = userDAO.findUser(email, password);
+		return user;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setUserDAO(UserDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+
+	public void setSession(Map<String, Object> session) {
+		this.sessionMap = session;
+	}
+
+}
