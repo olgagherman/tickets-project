e4c3b2d0b0828c47542f12ef1f3b6d4c297c534e
olgagherman <gherman.olya@gmail.com>
2016-03-13 21:56:57 +0200

    added entity ticket

---
+++ src/main/java/md/utm/fi/model/entity/Ticket.java
@@ -0,0 +1,45 @@
+package md.utm.fi.model.entity;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Ticket {
+	private Integer id;
+	private String name;
+	private String subject;
+	private Integer complexity;
+	private Date created;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public Integer getComplexity() {
+		return complexity;
+	}
+	public void setComplexity(Integer complexity) {
+		this.complexity = complexity;
+	}
+	public Date getCreated() {
+		return created;
+	}
+	public void setCreated(Date created) {
+		this.created = created;
+	}
+
+}
